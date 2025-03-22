5ddd60eef1f80530586bc7301edf1d0a298d3b76
AdeobaAdedayo18 <adeobaadedayo18@gmail.com>
2025-03-22 23:05:25 +0100

    Fixed routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import LandingPage2 from "./pages/LandingPage2";
-import LandingPage3 from "./pages/LandingPage3";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/2",
     element: <LandingPage2 />,
-  },
-  {
-    path: "/3",
-    element: <LandingPage3 />,
   },
 ]);
